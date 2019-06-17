0e2472f193b03f065a0f5e77e22f273b7fd7cf56
Toby Buckley <mobytoby@gmail.com>
2019-06-17 16:52:03 -0700

    Auth controller and helpers

---
+++ Crafted.Api/Util/Tokens.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Crafted.Api.Auth;
+using Crafted.Api.Models;
+using Newtonsoft.Json;
+
+namespace Crafted.Api.Util
+{
+    public class Tokens
+    {
+        public static async Task<string> GenerateJwt(ClaimsIdentity identity, IJwtFactory jwtFactory, string userName, JwtIssuerOptions jwtOptions, JsonSerializerSettings serializerSettings)
+        {
+            var response = new
+            {
+                id = identity.Claims.Single(c => c.Type == "id").Value,
+                auth_token = await jwtFactory.GenerateEncodedToken(userName, identity),
+                expires_in = (int)jwtOptions.ValidFor.TotalSeconds
+            };
+
+            return JsonConvert.SerializeObject(response, serializerSettings);
+        }
+    }
+}
---
+++ Crafted.DTO/CredentialsDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Crafted.DTO
+{
+    public class CredentialsDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
