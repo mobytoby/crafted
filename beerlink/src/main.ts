f4f53b9a78831d70776d8eeb5e2dd6507cc3c037
mobytoby <mobytoby@users.noreply.github.com>
2019-06-19 21:06:44 -0700

    client app

--- beerlink/src/main.ts
+++ beerlink/src/main.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Akveo 2019. All Rights Reserved.
+ * Licensed under the Personal / Commercial License.
+ * See LICENSE_PERSONAL / LICENSE_COMMERCIAL in the project root for license information on type of purchased license.
+ */
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
